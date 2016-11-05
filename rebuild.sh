88e3faecdc8f8ecc3dcc930993b6f9bf0bc08181
markm541374 <markm@robots.ox.ac.uk>
2016-11-05 16:02:36 +0000

    added modified only rebuild script

---
+++ rebuild.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+(cd gpbo/core;
+echo "rebuilding gpbo.core..."
+for FILE in *.pyx; do
+    NAME=$(echo $FILE | cut -f 1 -d '.');
+    if (( $(date +%s -r $FILE) > $(date +%s -r $"$NAME.c") ))
+    then
+        echo "rebuilding $NAME"
+        cython $FILE
+        gcc -shared -pthread -fPIC -fwrapv -O2 -Wall -fno-strict-aliasing -I/usr/include/python2.7 $"-I$HOME/.local/lib/python2.7/site-packages/numpy/core/include" -o $"$NAME.so" $"$NAME.c"
+
+    else
+        echo "$NAME unchanged"
+    fi
+    done
+
+)
