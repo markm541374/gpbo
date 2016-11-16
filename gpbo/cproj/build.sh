9235517b89c1e56489f0d49a0788542a69312340
markm541374 <markm@robots.ox.ac.uk>
2016-11-16 22:52:15 +0000

    got rid of clutter in gcc calls

--- gpbo/cproj/build.sh
+++ gpbo/cproj/build.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 
-g++ -std=c++11   -c -O2 -fPIC  -MMD -MP -MF "GPsimple.o.d" -o GPsimple.o GPsimple.cpp
+g++ -std=c++11   -c -O3 -fPIC  GPsimple.cpp
 
-g++ -std=c++11   -c -O2 -fPIC  -MMD -MP -MF "bayesutils.o.d" -o bayesutils.o bayesutils.cpp
+g++ -std=c++11   -c -O3 -fPIC  bayesutils.cpp
 
-g++ -std=c++11   -c -O2 -fPIC  -MMD -MP -MF "direct.o.d" -o direct.o direct.cpp
+g++ -std=c++11   -c -O3 -fPIC  direct.cpp
 
-g++ -std=c++11   -c -O2 -fPIC  -MMD -MP -MF "hypsearch.o.d" -o hypsearch.o hypsearch.cpp
+g++ -std=c++11   -c -O3 -fPIC  hypsearch.cpp
 
-g++ -std=c++11   -c -O2 -fPIC  -MMD -MP -MF "kernel.o.d" -o kernel.o kernel.cpp
+g++ -std=c++11   -c -O3 -fPIC  kernel.cpp
 
-g++ -std=c++11   -c -O2 -fPIC  -MMD -MP -MF "libGP.o.d" -o libGP.o libGP.cpp
+g++ -std=c++11   -c -O3 -fPIC  libGP.cpp
 
-g++ -std=c++11   -c -O2 -fPIC  -MMD -MP -MF "matern.o.d" -o matern.o matern.cpp
+g++ -std=c++11   -c -O3 -fPIC  matern.cpp
 
-g++ -std=c++11   -c -O2 -fPIC  -MMD -MP -MF "misctools.o.d" -o misctools.o misctools.cpp
+g++ -std=c++11   -c -O3 -fPIC  misctools.cpp
 
-g++ -std=c++11   -c -O2 -fPIC  -MMD -MP -MF "newmain.o.d" -o newmain.o newmain.cpp
+g++ -std=c++11   -c -O3 -fPIC  newmain.cpp
 
-g++ -std=c++11   -c -O2 -fPIC  -MMD -MP -MF "simplekernels.o.d" -o simplekernels.o simplekernels.cpp
+g++ -std=c++11   -c -O3 -fPIC  simplekernels.cpp
 
 g++ -std=c++11    -o libcproj.so GPsimple.o bayesutils.o direct.o hypsearch.o kernel.o libGP.o matern.o misctools.o newmain.o simplekernels.o -lblas -llapack -llapacke -shared -fPIC
