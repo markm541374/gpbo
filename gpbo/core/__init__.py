df65c79c8bbd1e866795d1aaba31fa5a38df90d2
markm541374 <markm@robots.ox.ac.uk>
2017-01-16 14:03:31 +0000

    rename optutils

--- gpbo/core/__init__.py
+++ gpbo/core/__init__.py
@@ -3,10 +3,10 @@ debugpath='dbout'
 debugoptions={'datavis':True,'drawlap':True,'cost1d':True,'ctaq':True}
 
 from .optimize import *
+from .optutils import *
 from .acquisitions import *
 from .reccomenders import *
 from .config import *
 from .GPdc import *
 from .ESutils import *
 from .choosers import *
-from .OPTutils import *
