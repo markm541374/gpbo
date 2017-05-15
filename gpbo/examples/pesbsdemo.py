db3d8a35fd5d4043c05968899394af0478f1f0e0
markm541374 <markm@robots.ox.ac.uk>
2017-05-15 14:01:13 +0100

    leanup examples

--- gpbo/examples/pesbsdemo.py
+++ gpbo/examples/pesbsdemo.py
@@ -1,27 +1,22 @@
-#optimize on draw from matern kernel
-
 import gpbo
-import gpbo.core.objectives as objectives
 import scipy as sp
 import copy
-import os
-import time
-from matplotlib import pyplot as plt
-import re
 
 
+
+#dimension
 D=2
-lb = [-1., -1.]
-ub = [1., 1.]
+#random initialization
+ninit=10
+#const diagonal noise variance
 s=1e-6
 
-
 def f(x, **ev):
-    #c = 1 - 0.5* ev['xa']
     c=45*sp.exp(-10.*ev['xa'])
     y = -sp.cos(x[0]) - sp.cos(x[1]) + 2.
     b = 0.1*ev['xa'] ** 2
     n = sp.random.normal() * sp.sqrt(s)
+    #this is used to check the true value at the imcumbent if available for plotting. Not counted in the optimization
     if 'cheattrue' in ev.keys():
         if ev['cheattrue']:
             b = 0
@@ -37,9 +32,10 @@ def f_inplane(x,**ev):
     return y,c,aux
 
 
-
+#pesbs is the predictive entropy search with biased variable cost observations as detailed in https://arxiv.org/abs/1703.04335
 C=gpbo.core.config.pesbsdefault(f,D,50,s,'results','pesbsdemo.csv')
-C.stoppara = {'tmax': 60 * 60*3}
+C.aqpara['nrandinit']=ninit
+C.stoppara = {'tmax': 60 * 60*8}
 C.stopfn = gpbo.core.optimize.totaltstopfn
 C.aqpara['overhead']='predict'
 out = gpbo.search(C)
--- gpbo/examples/pesfsdemo.py
+++ gpbo/examples/pesfsdemo.py
@@ -2,25 +2,28 @@ import gpbo
 import scipy as sp
 
 
-gpbo.core.debugoutput['datavis']=True
-
-
+#dimension
 D=2
-n=50
+#random initialization
+ninit=10
+#const diagonal noise variance
 s=1e-6
+
 def f(x,**ev):
 
     y=-sp.cos(x[0])-sp.cos(x[1])+2
     c=1.
-    n = sp.random.normal()*1e-3
+    n = sp.random.normal()*sp.sqrt(s)
+    #this is used to check the true value at the imcumbent if available for plotting. Not counted in the optimization
     if 'cheattrue' in ev.keys():
         if ev['cheattrue']:
             n=0
     print 'f inputs x:{} ev:{} outputs y:{} (n:{}) c:{}'.format(x,ev,y+n,n,c)
     return y+n,c,dict()
 
-
-C=gpbo.core.config.pesfsdefault(f,D,n,s,'results','pesfs.csv')
-#C.aqfn = gpbo.core.acquisitions.vmaxaq
+nsteps=50
+#pes with constant known noise
+C=gpbo.core.config.pesfsdefault(f,D,nsteps,s,'results','pesfs.csv')
+C.aqpara['nrandinit']=ninit
 out = gpbo.search(C)
 print out
