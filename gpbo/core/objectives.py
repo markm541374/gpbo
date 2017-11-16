rotation in eihyp, fix ratated pass to local

gpbo/core/objectives.py
@@ -75,8 +75,35 @@ def shifthart3D(x,**ev):
         F.append((fp-fn)/(2.*h))
     return F,c,d
 
-hart6min = -3.32237
-hart6xmin = [0.20169,0.150011,0.476874,0.275332,0.311652,0.6573]
+hart4min = -3.1344941412223984440892
+def shifthart4(x, **ev):
+    #hartmann4 with a linear offset agains quadratic cost
+    z = [0.5*xi +0.5 for xi in x]
+    al = sp.array([1.,1.2,3.,3.2]).T
+    A = sp.array([[10., 3., 17., 3.5, 1.7, 8.],
+                  [0.05, 10., 17., 0.1, 8, 14.],
+                  [3., 3.5, 1.7, 10., 17., 8.],
+                  [17., 8., 0.05, 10., 0.1, 14.]])
+    P = 0.0001 * sp.array([[1312., 1696., 5569., 124., 8283., 5886.],
+                           [2329., 4135., 8307., 3736., 1004., 9991.],
+                           [2348., 1451., 3522., 2883., 3047., 6650.],
+                           [4047., 8828., 8732., 5743., 1091., 381.]])
+    outer = 0
+    for ii in range(4):
+        inner = 0
+        for jj in range(4):
+            xj = z[jj]
+            Aij = A[ii, jj]
+            Pij = P[ii, jj]
+            inner += Aij * (xj - Pij)**2
+
+        new = al[ii] * sp.exp(-inner)
+        outer = outer + new
+    f = (1.1-outer)/0.839
+
+    #print( 'f inputs x:{} ev:{} outputs y:{}  '.format(z, ev, f))
+    return f-hart4min, 1., dict()
+hart6min = -3.3223680114155138112
 def shifthart6(x, **ev):
     #hartmann4 with a linear offset agains quadratic cost
     z = [0.5*xi +0.5 for xi in x]
@@ -102,7 +129,7 @@ def shifthart6(x, **ev):
         outer = outer + new
     f = -outer
 
-    print( 'f inputs x:{} ev:{} outputs y:{}  '.format(z, ev, f))
+    #print( 'f inputs x:{} ev:{} outputs y:{}  '.format(z, ev, f))
     return f-hart6min, 1., dict()
 
 def rosenojf(x,**ev):

gpbo/test/testobjective.py
@@ -3,17 +3,17 @@ import scipy as sp
 import numpy as np
 
 from scipy.optimize import minimize
-f = lambda x:gpbo.core.objectives.shifthart3(x,**{})[0]
+f = lambda x:gpbo.core.objectives.shifthart4(x,**{})[0]
 #x0 = np.array([0.114614, 0.555649, 0.852547])*2-1
-x0 = np.array([-0.77082223945184114, 0.11129778050761265, 0.70509396176578065])
+x0 = np.array([-0.62520946, -0.61169694,  0.11583556, -0.47044076])
 print(f(x0))
 res=minimize(f,x0,tol=1e-20)
 print(res)
 ymin = f(x0)
-#for i in range(1000):
-#    X = x0+np.random.normal(size=3,scale=1e-5,loc=0)
-#    y=f(X)
- #   if ymin>y:
- #       print(y,X)
- ##       ymin=y
- #       Xmin=X
+for i in range(1000):
+    X = x0+np.random.normal(size=4,scale=1e-8,loc=0)
+    y=f(X)
+    if ymin>y:
+        print(y,X)
+        ymin=y
+        Xmin=X

gpbo/test/tmp.py
@@ -2,8 +2,8 @@ import numpy as np
 import scipy as sp
 import gpbo
 import pandas as pd
-
-df = pd.read_csv('/home/mark/gpbo/exps/stopping/colville/results/s_2.csv')
+#gpbo.core.debugoutput['tmp']=True
+df = pd.read_csv('/home/mark/gpbo/exps/stopping/colville/results/s_3.csv')
 dim=4
 X = np.vstack([df[' x{}'.format(i)].values for i in range(dim)]).T
 Y = np.array([df[' y'].values]).T
@@ -11,18 +11,23 @@ print(Y.min())
 S = np.array([[0.]*Y.size]).T
 D=[[np.NaN]]*Y.size
 O = gpbo.core.optimize.optstate()
-n=50
+n=10
 for i in range(n):
     O.update(X[i,:],{'d':[np.NaN],'s':0.},Y[i,0],1.,1.)
-#C = gpbo.core.config.eihypdefault(lambda x:0,2,10,0.,'','')
-C=gpbo.core.config.switchdefault(lambda x:0,dim,10,250,0.,'','')
+C = gpbo.core.config.eihypdefault(lambda x:0,4,10,0.,'','')
+#C=gpbo.core.config.switchdefault(lambda x:0,dim,10,250,0.,'','')
 
 R=np.array([[-0.39400595, -0.02694207, -0.19059855, -0.89872444],
  [ 0.87676298, -0.04119442,  0.21425891, -0.42858232],
  [ 0.23588363, -0.43604394, -0.8634903,   0.09278525],
  [-0.1428457,  -0.89857822,  0.41490941,  0.00156942]])
-persist = {'n':n,'d':dim,'flip':False,'raiseS':False,'R':R}
+persist = {'n':n,'d':dim,'flip':False,'raiseS':False}
 #C.aqpara['choosereturn']={'offsetEI':0}
-#gpbo.core.acquisitions.eihypaq(O,persist,**C.aqpara)
-x = gpbo.core.choosers.globallocalregret(O,persist,**C.choosepara)
-print(0)
+x,par,per,aux = gpbo.core.acquisitions.eihypaq(O,persist,**C.aqpara)
+x1,par,per,aux = gpbo.core.acquisitions.eihypaq(O,persist,**C.aqpara)
+persist['R']=R
+xr,par,per,aux = gpbo.core.acquisitions.eihypaq(O,persist,**C.aqpara)
+#x = gpbo.core.choosers.globallocalregret(O,persist,**C.choosepara)
+print(x)
+print(x1)
+print(xr)