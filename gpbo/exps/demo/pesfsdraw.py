3f72023e39bdfb307148a8901ab8ae7a515cb8f3
markm541374 <mark.mcleod.17@googlemail.com>
2018-05-16 16:44:45 +0100

    remove irrelevant

--- gpbo/exps/demo/pesfsdraw.py
+++
@@ -1,9 +0,0 @@
-import gpbo
-import scipy as sp
-
-D=4
-s=1e-6
-f,xmin,ymin = gpbo.core.objectives.genmat52ojf(D,[-1.]*D,[1.]*D,1.,[0.5]*D)
-C=gpbo.core.config.pesfsdefault(f,D,80,s,'results','pesfs4d.csv')
-out = gpbo.search(C)
-print(out)
